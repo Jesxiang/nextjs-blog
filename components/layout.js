f55bf32218be1e7657210de87a4569551b1393c0
Jeszhang <zhangxiang4@dgg.net>
2023-07-27 14:29:33 +0800

    feat: nextjs-blog项目

---
+++ components/layout.js
@@ -0,0 +1,56 @@
+import styles from './layout.module.css'
+import utilsStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Head from 'next/head'
+
+const name = 'Jeszhang'
+export const siteTitle = "Next.js Sample Website"
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href='/favicon.ico'></link>
+        <meta name="description" content="Learn how to build a personal website using Next,js"></meta>
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle}></meta>
+        <meta name="twitter:card" content='summary_large_image'></meta>
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img src='/images/profile.jpg' className={`${styles.headerHomeImage} ${utilsStyles.borderCircle}`} alt={name} />
+            <h1 className={ utilsStyles.heading2Xl }>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <img src="/images/profile.jpg" className={`${styles.headerImage} ${utilsStyles.borderCircle}`} alt={name} />
+              </a>
+            </Link>
+            <h2 className={utilsStyles.headingLg}>
+              <Link href="/">
+                <a className={utilsStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {
+        !home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a> Back to home</a>
+            </Link>
+          </div>
+        )
+      }
+  </div>
+  )
+}
---
+++ posts/pre-rendering.md
@@ -0,0 +1,17 @@
+---
+title: 'Two Forms of Pre-rendering'
+date: '2020-01-01'
+---
+
+Next.js has two forms of pre-rendering: **Static Generation** and **Server-side Rendering**. The difference is in **when** it generates the HTML for a page.
+
+- **Static Generation** is the pre-rendering method that generates the HTML at **build time**. The pre-rendered HTML is then _reused_ on each request.
+- **Server-side Rendering** is the pre-rendering method that generates the HTML on **each request**.
+
+`Importantly`, Next.js lets you **choose** which pre-rendering form to use for each page. You can create a "hybrid" Next.js app by using Static Generation for most pages and using Server-side Rendering for others.
+
+
+1. 过来就回家回家
+2. 交换机和经济很快
+3. 呵呵呵呵让他也日益
+4. 的规划的获得丰厚23
